f032a3f712a18d2ad7cb1ffda58f41a85963d6d0
wangc <1520773319@qq.com>
2025-05-16 17:27:02 +0800

    增加mtrace控制宏

--- src/memory/paddr.c
+++ src/memory/paddr.c
@@ -51,14 +51,22 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  if (likely(in_pmem(addr))) {
+    word_t data = pmem_read(addr, len);
+    IFDEF(CONFIG_MTRACE,printf("[%-11s] addr: 0x%x  length: %d  data: 0x%x\n", __FUNCTION__, addr, len, data));
+    return data;
+  }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  if (likely(in_pmem(addr))) {
+    pmem_write(addr, len, data); 
+    IFDEF(CONFIG_MTRACE, printf("[%-11s] addr: 0x%x  length: %d  data: 0x%x\n", __FUNCTION__, addr, len, data));
+    return; 
+  }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
