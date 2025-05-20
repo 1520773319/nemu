57e4313b8812fa985e6ae23edc240b4a47d70cbc
wangc <1520773319@qq.com>
2025-05-20 17:19:42 +0800

    为ftrace曾加控制宏

--- src/cpu/cpu-exec.c
+++ src/cpu/cpu-exec.c
@@ -71,7 +71,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst, ilen);
 
   iring(p, s->isa.inst, MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc));
-  ftrace(s, p);
+  IFDEF(CONFIG_FTRACE, ftrace(s, p));
 #endif
 }
 
